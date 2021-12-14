d624a16e064c00d050b224fae954a6a5dae2b630
Gary Kaufman <gary.kaufman@ibm.com>
2021-12-14 15:07:07 -0600

    Changed to locally hosted db

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Database Connection
 try {
-    mongoose.connect(process.env.DB_URI);
-    console.log("Connected to DB!")
+  mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true });
+  console.log("Connected to DB!");
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
-db = mongoose.connection
+db = mongoose.connection;
 
 // Express Server
 const app = express();
 app.listen(3000, () => {
-    console.log("Listening on port 3000!");
-})
+  console.log("Listening on port 3000!");
+});
 app.use(express.json());
 
 // Routers
-const postsRouter = require('./routes/posts');
-app.use('/posts', postsRouter);
-
-
-
+const postsRouter = require("./routes/posts");
+app.use("/posts", postsRouter);
