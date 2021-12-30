65ab17d1a8ff5aa1e6c653e0214618aa2ea37275
Gary Kaufman <gary.kaufman@ibm.com>
2021-12-30 16:05:04 -0600

    Fixed JWT authorization issues

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const cors = require("cors");
-const app = express();
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const cors = require("cors")
+const app = express()
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 // Authenticate Token!
 module.exports = authenticateToken = function (req, res, next) {
-  const authHeader = req.headers["authorization"];
-  console.log(authHeader);
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) {
-    return res.sendStatus(401);
-  } else {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
-      if (err) {
-        return res.sendStatus(403);
-      } else {
-        req.username = username;
-        next();
-      }
-    });
-  }
-};
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+    if (token == null) {
+        return res.sendStatus(401)
+    } else {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
+            if (err) {
+                return res.sendStatus(403)
+            } else {
+                req.username = username
+                next()
+            }
+        })
+    }
+}
 
 // Database Connection
 try {
-  mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true });
-  console.log("Connected to DB!");
+    mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true })
+    console.log("Connected to DB!")
 } catch (err) {
-  console.log(err);
+    console.log(err)
 }
-db = mongoose.connection;
+db = mongoose.connection
 
 // Express Server
-app.use(cors());
+app.use(cors())
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
-});
-app.use(express.json());
+    console.log("Listening on port 3000!")
+})
+app.use(express.json())
 
 // Routers
-const postsRouter = require("./routes/posts");
-app.use("/posts", postsRouter);
+const postsRouter = require("./routes/posts")
+app.use("/posts", postsRouter)
 
-const authRouter = require("./routes/auth");
-app.use("/auth", authRouter);
+const authRouter = require("./routes/auth")
+app.use("/auth", authRouter)
--- routes/auth.js
+++ routes/auth.js
@@ -1,65 +1,90 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const authenticateToken = require("../app");
+const express = require("express")
+const router = express.Router()
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const authenticateToken = require("../app")
 
 // Get list of users and passwords
 // Not for production!!!
 router.get("/", authenticateToken, async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+    try {
+        res.status(200).json({ message: "You are logged in!" })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 // Register a new user
 router.post("/register", async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10); // "10" parameter produces salt with default value 10
-  const user = new User({
-    username: req.body.username,
-    password: hashedPassword,
-  });
-  try {
-    const newUser = await user.save();
-    res.status(201).send("User registered!");
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+    const hashedPassword = await bcrypt.hash(req.body.password, 10) // "10" parameter produces salt with default value 10
+    const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+    })
+    try {
+        const newUser = await user.save()
+        res.status(201).send("User registered!")
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Should be modified to a Redis cache or held in database later
+let refreshTokens = []
+
+// Check for refresh token, if valid, provide new access token
+router.post("/token", (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, username) => {
+            if (err) return res.sendStatus(403)
+            const accessToken = generateAccessToken(username)
+            res.json({ accessToken: accessToken })
+        }
+    )
+})
 
 // Authenticate a user, and provide them with access and refresh tokens
 router.post("/login", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user === null) {
-    return res.status(400).send("User not found");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      const username = req.body.username;
-      const accessToken = generateAccessToken(username);
-      const refreshToken = generateRefreshToken;
-      res.json({ accessToken: accessToken, refreshToken: refreshToken });
-    } else {
-      res.send("Password incorrect");
+    const user = await User.findOne({ username: req.body.username })
+    if (user === null) {
+        return res.status(400).send("User not found")
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const username = req.body.username
+            const accessToken = generateAccessToken(username)
+            const refreshToken = generateRefreshToken(username)
+            refreshTokens.push(refreshToken)
+            res.json({ accessToken: accessToken, refreshToken: refreshToken })
+        } else {
+            res.send("Password incorrect")
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Delete Refresh token will logout a user
+router.delete("/logout", (req, res) => {
+    refreshTokens = refreshTokens.filter((token) => token !== req.body.token)
+    res.sendStatus(204).json({ message: "You have successfully logged out." })
+})
 
 const generateAccessToken = (username) => {
-  return jwt.sign(username, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "10m",
-  });
-};
+    return jwt.sign({ username: username }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "20s",
+    })
+}
 
 const generateRefreshToken = (username) => {
-  return jwt.sign(username, process.env.REFRESH_TOKEN_SECRET);
-};
+    return jwt.sign(username, process.env.REFRESH_TOKEN_SECRET)
+}
 
-module.exports = router;
+module.exports = router
