bcd0e9f0d70f12e3f9bad3851f525ddd2b8cad14
Gary Kaufman <garykkaufman@gmail.com>
2022-01-12 11:45:36 -0600

    Posts are sorted descending order

--- routes/posts.js
+++ routes/posts.js
@@ -15,7 +15,7 @@ router.use(function (req, res, next) {
 // Getting all
 router.get("/", async (req, res) => {
     try {
-        const posts = await Post.find()
+        const posts = await Post.find().sort({ postDate: "desc" })
         res.status(200).json(posts)
     } catch (err) {
         res.status(500).json({ message: err.message })
